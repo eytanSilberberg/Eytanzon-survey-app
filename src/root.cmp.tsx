ee480005d981d16e1df05be09b658b4e70190b0a
Guy <guyelizarov@gmail.com>
2022-11-27 16:28:58 +0200

    test

--- src/root.cmp.tsx
+++ src/root.cmp.tsx
@@ -1,16 +1,22 @@
 
+import { useEffect, useRef } from 'react';
 import './assets/styles/style.scss';
+import { User } from './interface/user.interface';
 
 import { LoginSignup } from './pages/login-signup'
+import { useAppSelector } from './store/hooks';
+import { selectUser } from './store/user/userSlice';
 
 
 function App() {
-
-
-
+  
+  const userRef< = useRef()
+   userRef.current =  useAppSelector(selectUser)
   return (
     <div className="App">
-      <LoginSignup  />
+      <LoginSignup />
+      {user.length && <h1>{user[0].username}</h1>}
+
     </div>
   )
 }
